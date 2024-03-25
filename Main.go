5b9dd57f2d33bde3a54c0448c48dcfb3d07385a0
robin <Robin.Kroker@gmx.de>
2024-03-25 09:36:24 +0100

    added menu

--- Main.go
+++ Main.go
@@ -1,19 +1,128 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"runtime"
+	"strconv"
+	"strings"
 	"time"
 )
 
 func main() {
-	var pool = generateWorkerpool(6, "./resources", "password")
-	start := time.Now()
-	pool.run()
-	showBanner(start)
+	var path, _ = filepath.Abs(os.Args[1])
+	var numThreads = runtime.NumCPU() / 2
+	showBanner()
+
+	for {
+		fmt.Println("======================info======================")
+		printPathInfo(path)
+		printThreadInfo(numThreads)
+		printNumOfFiles(path)
+		choice := showMenuAndGetChoice(path)
+		if choice == 2 {
+			password := getPasswordFromUser()
+			var pool = generateWorkerpool(numThreads, path, password)
+			var start = time.Now()
+			fmt.Println("Running workers")
+			pool.run()
+			fmt.Println("Done after " + time.Since(start).String())
+			break
+		}
+		if choice == 1 {
+			numThreads = getNumOfThreadsFromUser()
+		}
+		if choice != 1 && choice != 2 {
+			os.Exit(0)
+		}
+	}
+
 }
 
-func showBanner(start time.Time) {
-	fmt.Println(time.Since(start))
+func printNumOfFiles(path string) {
+	var paths []string
+	paths = getAllPaths(path, paths)
+	fmt.Print("Number of files: ")
+	fmt.Print(len(paths))
+	fmt.Println()
+}
+
+func getNumOfThreadsFromUser() int {
+	fmt.Print("Please set the desired number of threads> ")
+	reader := bufio.NewReader(os.Stdin)
+	line, err := reader.ReadString('\n')
+	line = strings.ReplaceAll(line, "\n", "")
+	if err != nil {
+		log.Fatal(err)
+	}
+	num, err := strconv.Atoi(line)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return num
+}
+
+func getPasswordFromUser() string {
+	fmt.Print("Please set the desired password> ")
+	reader := bufio.NewReader(os.Stdin)
+	line, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
+	return line
+}
+
+func printThreadInfo(numThreads int) {
+	fmt.Print("Number of Threads: ")
+	fmt.Print(numThreads)
+	fmt.Println()
+}
+func showMenuAndGetChoice(path string) int {
+	fmt.Println("======================Menu======================")
+	if pathFolderContainsEncData(path) {
+		fmt.Println("2 => Decrypt")
+	} else {
+		fmt.Println("2 => Encrypt")
+	}
+	fmt.Println("1 => Change number of threads")
+	fmt.Println("0 => Exit")
+	fmt.Println()
+	fmt.Print("Please set the desired option> ")
+	reader := bufio.NewReader(os.Stdin)
+	line, err := reader.ReadString('\n')
+	line = strings.ReplaceAll(line, "\n", "")
+	if err != nil {
+		log.Fatal(err)
+	}
+	num, err := strconv.Atoi(line)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return num
+}
+
+func printPathInfo(path string) {
+	if pathFolderContainsEncData(path) {
+		fmt.Print("Folder to decrypt: ")
+		if len(path) > 20 {
+			fmt.Println("...." + path[len(path)-20:])
+		} else {
+			fmt.Println(path)
+		}
+	} else {
+		fmt.Print("Folder to encrypt: ")
+		if len(path) > 20 {
+			fmt.Println("...." + path[len(path)-20:])
+		} else {
+			fmt.Println(path)
+		}
+	}
+}
+
+func showBanner() {
 	fmt.Println("______    _     _             _____                            _")
 	fmt.Println("|  ___|  | |   | |           |  ___|                          | |")
 	fmt.Println("| |_ ___ | | __| | ___ _ __  | |__ _ __   ___ _ __ _   _ _ __ | |_ ___  _ __")
